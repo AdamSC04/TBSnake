8dfa022d8032167a2a78201ed570e3cc31ef8b49
AdamSC04 <adamsamuel.carlstrom@utb.helsingborg.se>
2022-12-06 09:33:34 +0100

    attributes + constructor

--- TBSnake/Snake.cs
+++ TBSnake/Snake.cs
@@ -12,6 +12,14 @@ namespace TBSnake
         public int length, x, y, direction;
         List<Point> body;
 
+        public Snake(int x, int y)
+        {
+            length = 1;
+            this.x = x;
+            this.y = y;
+            direction = 0;
+        }
+
         public void HandleInput(ConsoleKey key)
         {
 
