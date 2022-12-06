0e411835f42d6c0f0439a7c4c4f392504dcbaa31
ElectricSweden <olle.nagard@gmail.com>
2022-12-06 11:55:41 +0100

    Making Board with Coords

--- TBSnake/Game.cs
+++ TBSnake/Game.cs
@@ -9,8 +9,30 @@ namespace TBSnake
     internal class Game
     {
         private Snake snake;
-        private int width, height;
+        public List<Tuple<int, int>> _grid;
+        public int _gridSize;
 
+        public Game(int gridSize)
+        {
+            _gridSize = gridSize;
+        }
+
+        public void CreateBoard()
+        {
+            _grid = new List<Tuple<int, int>>();
+            int counter = 0;
+            for (int y = 0; y < _gridSize; y++)
+            {
+                for (int x = 0; x < _gridSize; x++)
+                {
+                    Tuple<int, int> tuple = new Tuple<int, int>(x, y);
+                    _grid.Add(tuple);
+                    Console.Write(_grid[counter]);
+                    counter++;
+                }
+                Console.WriteLine();
+            }
+        }
         // Spel loop
         public void Run()
         {
