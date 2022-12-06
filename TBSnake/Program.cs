e7840b8922cbb1a0005973e106478cc3396934a6
AdamSC04 <adamsamuel.carlstrom@utb.helsingborg.se>
2022-12-06 09:12:13 +0100

    baskod

--- TBSnake/Program.cs
+++ TBSnake/Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TBSnake
 {
     internal class Program
     {
+
+        protected class Snake
+        {
+            public int length, x, y, direction;
+            List<Point> body;
+
+            public void HandleInput(ConsoleKey key)
+            {
+
+            }
+
+            public void Step()
+            {
+
+            }
+
+            public void DrawStep()
+            {
+
+            }
+
+            public bool IsColliding()
+            {
+                return false;
+            }
+        }
+
+        protected class Game
+        {
+            private Snake snake;
+            private int width, height;
+
+            // Spel loop
+            public void Run()
+            {
+
+            }
+        }
+
         static void Main(string[] args)
         {
+
         }
     }
 }
