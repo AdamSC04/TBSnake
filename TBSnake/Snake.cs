ecae3dd193b263598c80ae61e88cdff5ad623e4c
Peter Enyedi <peterenyedi566@gmail.com>
2022-12-13 09:22:45 +0100

    input action

--- TBSnake/Snake.cs
+++ TBSnake/Snake.cs
@@ -23,7 +23,23 @@ namespace TBSnake
 
         public void HandleInput(ConsoleKey key)
         {
-
+            switch(key)
+            {
+                case ConsoleKey.RightArrow:
+                    direction = 0;
+                    break;
+                case ConsoleKey.DownArrow:
+                    direction = 1;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    direction = 2;
+                    break;
+                case ConsoleKey.UpArrow:
+                    direction = 3;
+                    break;
+                default:
+                    break;
+            }
         }
 
         public void Step()
